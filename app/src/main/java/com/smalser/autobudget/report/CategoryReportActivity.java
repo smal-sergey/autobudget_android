f63279b8a044130d8ee608cfaf4023d0d39b018b
Smal Sergey <smal.sergey@gmail.com>
2015-04-27 22:41:17 +0300

    Added header to category report view

--- app/src/main/java/com/smalser/autobudget/report/CategoryReportActivity.java
+++ app/src/main/java/com/smalser/autobudget/report/CategoryReportActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.smalser.autobudget.Category;
 import com.smalser.autobudget.Message;
@@ -18,6 +19,7 @@ import java.util.List;
 public class CategoryReportActivity extends Activity {
     public static final String CATEGORY_EXTRA = "category_extra";
     ListView mCategoryReport;
+    TextView mCategory;
     private Category category;
 
     @Override
@@ -25,8 +27,12 @@ public class CategoryReportActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_report);
 
-        mCategoryReport = (ListView) findViewById(R.id.listCategoryReport);
         category = Category.valueOf(getIntent().getStringExtra(CATEGORY_EXTRA));
+
+        mCategory = (TextView) findViewById(R.id.cr_lblCategory);
+        mCategory.setText(getResources().getString(category.lblId()));
+        mCategoryReport = (ListView) findViewById(R.id.listCategoryReport);
+
         final MyApplication app = (MyApplication) getApplication();
 
         mCategoryReport.setOnItemClickListener(new AdapterView.OnItemClickListener() {
